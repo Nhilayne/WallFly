ca3178433c5935ec311a09bec97bdf11da478ce8
Nhilayne <Nhilayne@gmail.com>
2024-07-25 18:06:36 -0700

    updated for pi replacement

--- server.py
+++ server.py
@@ -5,6 +5,12 @@ import select
 import hashlib
 from multiprocessing import pool
 import pandas as pd
+import uuid 
+
+def get_mac_address():
+    id = uuid.getnode()
+    id_formatted = ':'.join(('%012x'%id)[i:i+2] for i in range(1,12,2))
+    return id_formatted.lower()
 
 def privatize(mac):
     hashed = hashlib.sha256(mac.encode(), usedforsecurity=True).hexdigest()
@@ -16,7 +22,10 @@ def main():
     parser.add_argument("--address", "-a", default="localhost", help="IP for server")
     parser.add_argument("--port", "-p", default=8505, help="Listening Port")
     parser.add_argument("--database", "-d", help="Database source")
+    #parser.add_argument("--interface", "-i", default="wlan0", help="Network interface")
     args = parser.parse_args()
+    
+    
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((args.address, int(args.port)))
@@ -25,17 +34,26 @@ def main():
 
     connections = [server]
 
+    serverID = get_mac_address()
+
+    # (x,y,z) for an xy floorplan, z as depth
+    networkPositions = {
+        serverID:(0,0,0)
+    }
+
+
     input_set = pd.DataFrame(columns=['mac','rssi','time', 'ip'])
     input_set = input_set.sort_values(by=['mac', 'time'])
 
     while True:
         try:        
-            read_sockets,_,_ =select.select(connections,[],[],0)
+            read_sockets,_,_ = select.select(connections,[],[],0)
             for connection in read_sockets:
                 if connection == server:
                     client_connection, address = server.accept()
                     print(f'New connection from {address[0]}')
                     connections.append(client_connection)
+                    client_connection.send(networkPositions.keys.encode())
                 else:
                     msg = connection.recv(1024)
                     if not msg:
@@ -54,12 +72,13 @@ def main():
             pass
             # process_set = getNext(input_set)
             # location = calcPosition(process_set)
-            # storeLocation(location)h
+            # storeLocation(location)
 
 
         except KeyboardInterrupt:
             server.close()
             print(input_set.head(10))
+            print(networkPositions.keys)
             exit()
 
 
