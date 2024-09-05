6aeab123835810b5afd58eca4ce0d587cc41d8a9
Nhilayne <Nhilayne@gmail.com>
2024-09-05 00:50:48 -0700

    Add files via upload
    updated to decode dot11

--- cellsniff.py
+++ cellsniff.py
@@ -1,6 +1,5 @@
 from scapy.all import *
 from scapy.layers.dot11 import Dot11, RadioTap
-import struct
 import signal
 import sys
 
@@ -19,18 +18,22 @@ def get_rssi(packet):
 
 def process_packet(packet):
     """Process each packet to filter ICMP Echo Requests and capture RSSI"""
-    if packet.haslayer(IP) and packet.haslayer(ICMP):
-        ip_layer = packet.getlayer(IP)
-        icmp_layer = packet.getlayer(ICMP)
-        
-        # Check if it's an ICMP Echo Request (ping)
-        if icmp_layer.type == 8 and ip_layer.src == KNOWN_IP:
-            rssi = get_rssi(packet)
-            if rssi is not None:
-                print(f"ICMP Echo Request from {ip_layer.src} | RSSI: {rssi} dBm")
-                rssi_values.append(rssi)
-            else:
-                print(f"ICMP Echo Request from {ip_layer.src} | RSSI: Not available")
+    if packet.haslayer(Dot11):
+        # Check if it has a payload and if we can decode it to IP
+        if packet.type == 2:  # Data frame
+            raw_packet = packet[Dot11].payload
+            if raw_packet.haslayer(IP):
+                ip_layer = raw_packet.getlayer(IP)
+                icmp_layer = raw_packet.getlayer(ICMP)
+
+                # Check if it's an ICMP Echo Request (ping)
+                if icmp_layer.type == 8 and ip_layer.src == KNOWN_IP:
+                    rssi = get_rssi(packet)
+                    if rssi is not None:
+                        print(f"ICMP Echo Request from {ip_layer.src} | RSSI: {rssi} dBm")
+                        rssi_values.append(rssi)
+                    else:
+                        print(f"ICMP Echo Request from {ip_layer.src} | RSSI: Not available")
 
 def calculate_average_rssi():
     """Calculate and display the average RSSI when the program is terminated"""
@@ -51,4 +54,4 @@ signal.signal(signal.SIGINT, signal_handler)
 
 # Start sniffing on the wireless interface in monitor mode
 print(f"Sniffing on {iface}, waiting for ICMP Echo Requests from {KNOWN_IP}...")
-sniff(iface=iface, prn=process_packet, filter="icmp", store=0)
+sniff(iface=iface, prn=process_packet, store=0)
